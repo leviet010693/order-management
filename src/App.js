28e18bad083d30c63f7e43f8be383f2f39008ce2
Việt Lê <leviet010693@gmail.com>
2024-06-13 14:11:47 +0700

    [init] Bug: fix eslint complier

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Main from './components/layout/Main';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import './assets/styles/main.css';
 import './assets/styles/responsive.css';
 
